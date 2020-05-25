c5922eba3be6d3abbdca3f5cd23ca112c4c464fd
joyce <lishanhe@chainup.com>
2020-05-25 17:31:12 +0800

    源码测试

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -36,7 +36,7 @@ public class Consumer {
          * Instantiate with specified consumer group name.
          */
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_4");
-        consumer.setNamesrvAddr("127.0.0.1:9876");
+        consumer.setNamesrvAddr("127.0.0.1:9876");//指明 Consumer 使用的 RocketMQ Namesrv
         /*
          * Specify name server addresses.
          * <p/>
--- example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
@@ -32,7 +32,7 @@ public class Producer {
          * Instantiate with a producer group name.
          */
         DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
-        producer.setNamesrvAddr("127.0.0.1:9876");
+        producer.setNamesrvAddr("127.0.0.1:9876");//指明 Producer 使用的 RocketMQ Namesrv
         /*
          * Specify name server addresses.
          * <p/>
@@ -50,7 +50,7 @@ public class Producer {
          */
         producer.start();
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10; i++) {
             try {
 
                 /*
@@ -58,7 +58,7 @@ public class Producer {
                  */
                 Message msg = new Message("TopicTest" /* Topic */,
                     "TagA" /* Tag */,
-                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+                    ("Hello joyce" + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
                 );
 
                 /*
